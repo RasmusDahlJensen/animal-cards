5ed157b461da511aebaf3c145db15259429ce358
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-31 13:00:56 +0200

    123

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//Objects for animal data
+const myAnimalObject = {
+    dog: {name: "dog", img: "d", description: "o"},
+    cat: {name: "cat", img: "d", description: "o"},
+    echidna: {name: "echidna", img: "d", description: "o"},
+    hedgehog: {name: "hedgehog", img: "d", description: "o"}
+
+}
+
+
+//For...in loop for building my cards
+
+for (let myIndex in myAnimalObject){
+    // console.log(myAnimalObject[myIndex].name);
+    // console.log(myAnimalObject[myIndex].img);
+    // console.log(myAnimalObject[myIndex].description);
+
+    // buildCard(myAnimalObject[myIndex].name, myAnimalObject[myIndex].img, myAnimalObject[myIndex].description)
+
+}
+
+
+//Functions
+// buildCard(name, img, description){
+
+
+// }
+
+
+
+
+
+//Appends -> Keep at bottom.
